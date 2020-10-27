ac178ace0f3af989d6e14f15402f4d03ba99d421
Nick Xu <Nick.Xu@prudential.com.sg>
2020-10-27 14:50:03 +0800

    Add Proxy design pattern sample

---
+++ samples/Proxy-pattern/Proxy.playground/Pages/LazyLoad.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+class RandomIntWithID {
+    var value: Int = {
+        print("value initialized")
+        return Int.random(in: Int.min...Int.max)
+    }()
+    
+    lazy var uid: String = {
+        print("uid initialized")
+        return UUID().uuidString
+    }()
+}
+
+let n = RandomIntWithID()
+print(n.uid)
